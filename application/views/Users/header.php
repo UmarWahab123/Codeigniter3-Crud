f987e0918b2faf75e4143e8170e3899ca6879524
UmarWahab123 <umarwahab672@gmail.com>
2021-06-10 17:30:50 +0500

    my first codeigniter 3 crud

---
+++ application/views/Users/header.php
@@ -0,0 +1,31 @@
+<!-- <html>
+    <head>
+        <title>Article List</title>
+        <?=//link_tag("Assets/css/bootstrap.min.css") ?>
+        <style>
+       .btn{
+         background-color: lightgrey;
+       }
+        .container{
+          background-color:;
+        }
+        #two{
+            color: red;
+        }
+        </style>
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Article List</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit"><span id='two'>Search</span></button>
+    </form>
+  </div>
+</nav> -->
